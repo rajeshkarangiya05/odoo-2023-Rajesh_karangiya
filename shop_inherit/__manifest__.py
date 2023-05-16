bd4a458dde7ac876bfa2e7a84f02ecfaff7d14a4
rajesh <rajesh@icreativetechnolabs.com>
2023-05-16 18:53:47 +0530

    [updated]-shop_and_library

---
+++ shop_inherit/__manifest__.py
@@ -0,0 +1,13 @@
+{
+	
+	"name":"Shop Inherit",
+	"license":"LGPL-3",
+	"category":"Inherit",
+	"summary":"Shop Inherit",
+	"description":"Shop Inherit",
+	"depends":['website_sale'],
+	"data":[],
+	"installable":True,
+	"application":True,
+	"auto_install":False,
+}
