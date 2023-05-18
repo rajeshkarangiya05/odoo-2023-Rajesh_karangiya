7989171b93dbd5e0ba87d41d1a620306637b2849
rajesh <rajesh@icreativetechnolabs.com>
2023-05-18 19:07:18 +0530

    [ADD]-stock_inherit

---
+++ stock_inherit/__manifest__.py
@@ -0,0 +1,16 @@
+{
+	
+	"name":"Stock Inherit",
+	"license":"LGPL-3",
+	"category":"Inherit",
+	"summary":"Stock Inherit",
+	"description":"Stock Inherit",
+	"depends":['stock'],
+	"data":[
+		"report/stock_inherit_view.xml",
+
+	],
+	"installable":True,
+	"application":True,
+	"auto_install":False,
+}
